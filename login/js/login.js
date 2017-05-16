d51e6c967f1e660034c88a157bd830548ee786b5
Luciano <luciano.krebs@hotmail.com>
2017-05-16 17:33:23 -0300

    creating login page + plataform + plataform's page

---
+++ login/js/login.js
@@ -0,0 +1,28 @@
+alunos = [
+	"student01",
+];
+
+psw = [
+	"12345"
+];
+
+
+
+
+function testLogin() {
+	var a = document.getElementById('login').value;
+	var b = document.getElementById('senha').value;
+
+	for (var i = 0; i < alunos.length; i++) {
+		if (a === alunos[i] && b === psw[i]) {
+			window.open('../platform/platform.html');
+			document.getElementById('senha').value = "";
+			document.getElementById('login').value = "";
+		}
+		else {
+			document.getElementById('fail').style.display = 'inline-block';
+			document.getElementById('senha').value = "";
+			document.getElementById('login').value = "";
+		}	
+	}
+}

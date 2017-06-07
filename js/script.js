6031300b9a6c933aff89fade81800ca17ed3a245
Luciano <luciano.krebs@hotmail.com>
2017-06-07 20:25:23 -0300

    new features

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+	$("#goToDepoimentos").on('click', function() {
+	    $('html,body').animate({
+		    scrollTop: $("#depoimentos").offset().top
+		}, 2000);
+	});
+	$("#goToAulaExperimental").on('click', function() {
+		$('html, body').animate({
+			scrollTop: $("#aula-experimental").offset().top
+		}, 2000);
+	});
+	$("#goToMetodologia").on('click', function() {
+		$('html, body').animate({
+			scrollTop: $("#metodologia").offset().top
+		}, 1500);
+	});
+
+});

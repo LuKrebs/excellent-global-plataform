cc0fc59a876f357af36738dd15cc82e40026152b
Luciano <luciano.krebs@hotmail.com>
2017-06-12 22:31:29 -0300

    new features

--- login/js/login.js
+++ login/js/login.js
@@ -1,30 +1,45 @@
-alunos = [
-	"luciano",
-	"carol"
-];
+$(document).ready(function() {
+	var alunos = [
+		"Luciano",
+		"carol",
+		"aluno1",
+		"aluno2",
+		"aluno3",
+		"aluno4",
+		"aluno5",
+		"aluno6"
+	];
 
-psw = [
-	"12345",
-	"12345"
-];
+	var psw = [
+		"teacher12345",
+		"teacher12345",
+		"aluno1",
+		"aluno2",
+		"aluno3",
+		"aluno4",
+		"aluno5",
+		"aluno6"
+	];
 
+	$('form').submit(function() {
+		var a = document.getElementById('login').value;
+		var b = document.getElementById('senha').value;
 
-
-
-function testLogin() {
-	var a = document.getElementById('login').value;
-	var b = document.getElementById('senha').value;
-
-	for (var i = 0; i < alunos.length; i++) {
-		if (a === alunos[i] && b === psw[i]) {
-			window.open('../platform/platform.html');
-			document.getElementById('senha').value = "";
-			document.getElementById('login').value = "";
+		for (var i = 0; i < alunos.length; i++) {
+			if (a === alunos[i] && b === psw[i]) {
+				window.open('../platform/platform.html');
+				document.getElementById('senha').value = "";
+				document.getElementById('login').value = "";
+			}
+			else {
+				document.getElementById('fail').style.display = 'inline-block';
+				document.getElementById('senha').value = "";
+				document.getElementById('login').value = "";
+			}	
 		}
-		else {
-			document.getElementById('fail').style.display = 'inline-block';
-			document.getElementById('senha').value = "";
-			document.getElementById('login').value = "";
-		}	
-	}
-}
+		return false;
+	});
+	
+
+});
+
